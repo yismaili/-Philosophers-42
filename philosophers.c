2b5bf2fa584ba64178567097d6847da9699640d6
yo-ismaili <ismailiyounes20@gmail.com>
2022-04-22 01:38:54 +0000

    Started Philosophers

--- philosophers.c
+++ philosophers.c
@@ -55,24 +55,34 @@ int primes[10] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 void* routine(void* arg) {
     sleep(1);
     int index = *(int*)arg;
-    printf("%d ", primes[index]);
+    int i = 0;
+    int sum = 0;
+    while(i < 5)
+    {
+        sum += primes[index + i];
+        i++;
+    }
+    //printf("%d ", primes[index]);
     free(arg);
 }
 
 int main(int argc, char* argv[]) {
     pthread_t th[10];
-    int i;
-    for (i = 0; i < 10; i++) {
+    int i = 0;
+    while (i < 2) {
         int* a = malloc(sizeof(int));
-        *a = i;
+        *a = i * 5;
         if (pthread_create(&th[i], NULL, &routine, a) != 0) {
             perror("Failed to created thread");
         }
-    }
-    for (i = 0; i < 10; i++) {
+        i++;
+    } 
+    i = 0;
+    while (i < 2) {
         if (pthread_join(th[i], NULL) != 0) {
             perror("Failed to join thread");
         }
+        i++;
     }
     
     return 0;
